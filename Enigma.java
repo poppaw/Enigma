ae8f9b059dc072dcc8afcd9a293739d67a2acbf7
ejaworska <ewel.jaworska@gmail.com>
2018-03-26 13:13:54 +0200

    Add print_menu method

--- Enigma.java
+++ Enigma.java
@@ -3,13 +3,15 @@ import java.util.List;
 
 public class Enigma {
 
-  List<String> menu = new ArrayList<String>();
-  menu.add("Affine");
-  menu.add("Columnar Transposition Cipher");
+
 
 
   public static void main(String[] args) {
-    if (args[0].equals("-l")){
+    ArrayList<String> menu = new ArrayList<String>();
+    menu.add("Affine");
+    menu.add("Columnar Transposition Cipher");
+
+    if (args[0].equals("l")) {
       print_menu(menu);
     }
   }
@@ -17,16 +19,7 @@ public class Enigma {
   public static void print_menu(ArrayList<String> menu) {
     Integer index = 1;
     for (String cipher : menu) {
-      System.out.println(index + ")" + cipher);
+      System.out.println(index + ") " + cipher);
     }
-}
-
-
-
-
-
-
-
-
-
+  }
 }
