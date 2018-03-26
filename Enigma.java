ae5ecef8cc159a6ebfb4ec674da9af2563dd59da
ejaworska <ewel.jaworska@gmail.com>
2018-03-26 19:04:05 +0200

    Add affine cipher method

--- Enigma.java
+++ Enigma.java
@@ -3,9 +3,6 @@ import java.util.List;
 
 public class Enigma {
 
-
-
-
   public static void main(String[] args) {
     ArrayList<String> menu = new ArrayList<String>();
     menu.add("Affine");
@@ -22,4 +19,39 @@ public class Enigma {
       System.out.println(index + ") " + cipher);
     }
   }
+
+  public static void choose_cipher() {
+    System.out.println("Choose the cipher: ");
+
+  }
+
+  public static void affine(String text, String mode, ArrayList<Integer>key) {
+    text = text.replaceAll("\\s+","");
+    List<Character> cipher_result = new ArrayList<Character>();
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    Integer a = key.get(0);
+    Integer b = key.get(1);
+    for (Character ch: text.toCharArray()) {
+      if (mode.equals("e")) {
+        Integer x = alphabet.indexOf(ch);
+        Integer c = a*x + b;
+        Character encipher_char = alphabet.charAt(c%26);
+        cipher_result.add(encipher_char);
+      } else if (mode.equals("d")) {
+        Integer x = alphabet.indexOf(ch);
+        Integer c = 15*(x - 5);
+        Character decipher_char = alphabet.charAt(c%26);
+        cipher_result.add(decipher_char);
+      }
+    }
+    for (Character element : cipher_result) {
+      System.out.print(element);
+    }
+  }
+
+
+
+
+
+
 }
