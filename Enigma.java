46e41d3a007e9ff7214ada7b3a5639c0a18b148d
ejaworska <ewel.jaworska@gmail.com>
2018-03-27 10:42:22 +0200

    Add variables to print_menu

--- Enigma.java
+++ Enigma.java
@@ -4,16 +4,17 @@ import java.util.List;
 public class Enigma {
 
   public static void main(String[] args) {
-    ArrayList<String> menu = new ArrayList<String>();
-    menu.add("Affine");
-    menu.add("Columnar Transposition Cipher");
+
 
     if (args[0].equals("l")) {
-      print_menu(menu);
+      print_menu();
     }
   }
 
-  public static void print_menu(ArrayList<String> menu) {
+  public static void print_menu() {
+    ArrayList<String> menu = new ArrayList<String>();
+    menu.add("Affine");
+    menu.add("Columnar Transposition Cipher");
     Integer index = 1;
     for (String cipher : menu) {
       System.out.println(index + ") " + cipher);
