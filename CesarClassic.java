37029e9eb60e26d171800bcb3a3ae9b8df79275e
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-04-01 23:42:02 +0200

    CesarClassic and Rail merged with Pawel

--- CesarClassic.java
+++ CesarClassic.java
@@ -7,22 +7,42 @@ public class CesarClassic{
         for(int i=0; i< text.length(); i++){
             sign = text.charAt(i);
             if (!Character.isLetter(sign)) newSign = sign; //(sign == ' ')
-            else if ((sign<='Z') && (sign + shift > 'Z')) newSign = (char) (sign+shift - 26);
-            else if ((sign>='A') && ((sign + shift) < 'A')) newSign = (char) (sign + shift + 26);
-            else if ((sign<='z') && (sign + shift > 'z')) newSign = (char) (sign+shift - 26);
-            else if ((Character.isLowerCase(sign))&&(sign<='z') && (sign + shift < 'a')) newSign = (char) (sign + shift + 26);
+            else if (Character.isUpperCase(sign) && (sign + shift > 'Z')) newSign = (char) (sign+shift - 26);
+            else if (Character.isUpperCase(sign) && (sign + shift < 'A')) newSign = (char) (sign + shift + 26);
+            else if (Character.isLowerCase(sign) && (sign + shift > 'z')) newSign = (char) (sign+shift - 26);
+            else if (Character.isLowerCase(sign) && (sign + shift < 'a')) newSign = (char) (sign + shift + 26);
             else newSign = (char)(sign + shift);
-            
             encrypted += newSign;
         }
         return encrypted;
     }
+    public static String decrypt(String text, int shift){
+        char sign;
+        char newSign;
+        String decrypted = "";
+        for(int i=0; i< text.length(); i++){
+            sign = text.charAt(i);
+            if (!Character.isLetter(sign)) newSign = sign;
+            else if (Character.isUpperCase(sign) && (sign - shift > 'Z')) newSign = (char) (sign - shift + 26);
+            else if (Character.isUpperCase(sign) && (sign - shift < 'A')) newSign = (char) (sign - shift + 26); //-
+            else if (Character.isLowerCase(sign) && (sign - shift > 'z')) newSign = (char) (sign - shift + 26);
+            else if (Character.isLowerCase(sign) && (sign - shift < 'a')) newSign = (char) (sign - shift + 26); //-
+            else newSign = (char)(sign - shift);
+            decrypted += newSign;
+        }
+        return decrypted;
+    }
+        
 
     public static void main(String[] args) {
-        String text = "Bociany na Ekrany!";   
+        String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz !@#$%^&";   
         int shift = 8;
-        
-        System.out.println(encrypt(text,shift));
+        String encrypted = encrypt(text,shift);
+        System.out.println(encrypted);
+        String decrypted = decrypt(encrypted, shift);
+        System.out.println(decrypted);
+        boolean accurate = decrypted.equals(text);
+        System.out.println("decrypted = text before encryption: " + accurate);
 
         
     }
---
+++ RailFence.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.ArrayList;
+import static java.lang.System.out;
+
+public class RailFence{
+    
+    public static String railEncrypt(String text, int key){
+        List <char[]> rail = new ArrayList<char[]>();
+        //create  empty arrays nested in list
+        for(int i=0; i <key;i++) rail.add(new char[text.length()]);
+
+        int col; // position of element in array: depending on position of letter in text
+        int raw = 0; // oscilating (rising and falling) position of array in list within the list's length: from 0 to lenth of list(excluding).
+        int operand = 1; //changes raw value
+        for (int j=0; j<text.length();j++){
+            char sign = text.charAt(j);
+            col = j;
+            if(raw+operand<0 || raw+operand==key){// reaching list borders
+                operand = -operand; //changes oscylation direction
+            }
+            rail.get(raw)[col] = sign;
+            raw += operand;
+        }
+        String cipher = "";
+        for(char[]array:rail) {
+            for(char letter:array){
+                cipher += letter;
+            }
+        }
+        return cipher;
+    }
+
+    public static void main(String[] args) {
+        String text = new String("Hello World!");
+        int key = 3;
+        out.println(railEncrypt(text,key));
+    }
+}               
