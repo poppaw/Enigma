5b5906a4fb4702e89e75b6d2d4db9292514850f0
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-04-09 01:51:25 +0200

    add isLetter method to operate the CTC

--- CesarClassic.java
+++ CesarClassic.java
@@ -1,23 +1,22 @@
 public class CesarClassic{
-
-    public static String runCesarEncrypt(String text, int shift){
+    
+    public static String encrypt(String text, int shift) throws NumberFormatException{
         char sign;
         char newSign;
         String encrypted = "";
         for(int i=0; i< text.length(); i++){
             sign = text.charAt(i);
-            if (!Character.isLetter(sign)) newSign = sign; //(sign == ' ')
-            else if (Character.isUpperCase(sign) && (sign + shift > 'Z')) newSign = (char) (sign + shift - 26);
+            if (!Character.isLetter(sign)) newSign = sign;
+            else if (Character.isUpperCase(sign) && (sign + shift > 'Z')) newSign = (char) (sign+shift - 26);
             else if (Character.isUpperCase(sign) && (sign + shift < 'A')) newSign = (char) (sign + shift + 26);
-            else if (Character.isLowerCase(sign) && (sign + shift > 'z')) newSign = (char) (sign + shift - 26);
+            else if (Character.isLowerCase(sign) && (sign + shift > 'z')) newSign = (char) (sign+shift - 26);
             else if (Character.isLowerCase(sign) && (sign + shift < 'a')) newSign = (char) (sign + shift + 26);
             else newSign = (char)(sign + shift);
             encrypted += newSign;
         }
         return encrypted;
     }
-    
-    public static String runCesarDecrypt(String text, int shift){
+    public static String decrypt(String text, int shift)throws NumberFormatException{
         char sign;
         char newSign;
         String decrypted = "";
@@ -25,26 +24,24 @@ public class CesarClassic{
             sign = text.charAt(i);
             if (!Character.isLetter(sign)) newSign = sign;
             else if (Character.isUpperCase(sign) && (sign - shift > 'Z')) newSign = (char) (sign - shift + 26);
-            else if (Character.isUpperCase(sign) && (sign - shift < 'A')) newSign = (char) (sign - shift + 26); //-
+            else if (Character.isUpperCase(sign) && (sign - shift < 'A')) newSign = (char) (sign - shift + 26);
             else if (Character.isLowerCase(sign) && (sign - shift > 'z')) newSign = (char) (sign - shift + 26);
-            else if (Character.isLowerCase(sign) && (sign - shift < 'a')) newSign = (char) (sign - shift + 26); //-
+            else if (Character.isLowerCase(sign) && (sign - shift < 'a')) newSign = (char) (sign - shift + 26); 
             else newSign = (char)(sign - shift);
             decrypted += newSign;
         }
         return decrypted;
     }
-
+        
 
     public static void main(String[] args) {
-        String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz !@#$%^&";
+        String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz !@#$%^&";   
         int shift = 8;
-        String encrypted = runCesarEncrypt(text,shift);
+        String encrypted = encrypt(text,shift);
         System.out.println(encrypted);
-        String decrypted = runCesarDecrypt(encrypted, shift);
+        String decrypted = decrypt(encrypted, shift);
         System.out.println(decrypted);
         boolean accurate = decrypted.equals(text);
         System.out.println("decrypted = text before encryption: " + accurate);
-
-
     }
 }
